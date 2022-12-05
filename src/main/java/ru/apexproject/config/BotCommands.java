4bda0b4f6a230a673c4aa2be596825a4ffe31656
Pavel Basmanov <pavel.basmanov@apex-project.ru>
2022-12-05 19:03:30 +0300

    BotCommands added

---
+++ src/main/java/ru/apexproject/config/BotCommands.java
@@ -0,0 +1,9 @@
+package ru.apexproject.config;
+
+public class BotCommands {
+    public static final String CREATE_TASK = "#task";
+    public static final String REGISTER_DATABASE = "#register";
+    public static final String REREGISTER_DATABASE = "#reregister";
+    public static final String DELETE_DATABASE = "#delete";
+
+}
