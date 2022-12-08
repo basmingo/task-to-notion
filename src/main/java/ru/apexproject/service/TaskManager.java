ec43d71a4488cf00c9bbac7ff0ce0cde2ed72171
Pavel Basmanov <pavel.basmanov@apex-project.ru>
2022-12-08 21:44:36 +0300

    TaskManager added

---
+++ src/main/java/ru/apexproject/service/TaskManager.java
@@ -0,0 +1,65 @@
+package ru.apexproject.service;
+
+import ru.apexproject.config.BotCommands;
+import ru.apexproject.dto.notion.Task;
+import ru.apexproject.config.ApplicationConfig;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class TaskManager {
+    ChatService chatService;
+    NotionService notionService;
+    ApplicationConfig botCommands;
+    String description;
+    List<String> assignees;
+
+    public TaskManager(ChatService chatService,
+                       NotionService notionService,
+                       ApplicationConfig applicationConfig) {
+
+        this.chatService = chatService;
+        this.notionService = notionService;
+        this.botCommands = applicationConfig;
+    }
+
+    public void createTask(Supplier<Stream<String>> messageStream, String projectName) {
+        adjustParameters(messageStream);
+        Task task = new Task(
+                description,
+                assignees,
+                chatService.getChatDbMap().get(projectName));
+
+        notionService.doPost(task);
+    }
+
+    public void createTask(
+            Supplier<Stream<String>> messageStream,
+            String projectName,
+            String photo) {
+
+        adjustParameters(messageStream);
+        Task task = new Task(
+                description,
+                assignees,
+                photo,
+                chatService.getChatDbMap().get(projectName));
+        notionService.doPost(task);
+    }
+
+    private void adjustParameters(Supplier<Stream<String>> messageStream) {
+        description = messageStream
+                .get()
+                .filter(word -> word.charAt(0) != '@')
+                .filter(word -> !word.equals(BotCommands.CREATE_TASK))
+                .collect(Collectors.joining(" "));
+
+        assignees = messageStream
+                .get()
+                .filter(word -> word.charAt(0) == '@')
+                .collect(Collectors.toList());
+
+        if (assignees.isEmpty()) assignees.add("@all");
+    }
+}
