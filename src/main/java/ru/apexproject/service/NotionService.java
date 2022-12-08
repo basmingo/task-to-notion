af6232cc540612c0eebe84178bc9853672922bc7
Pavel Basmanov <pavel.basmanov@apex-project.ru>
2022-12-08 21:43:32 +0300

    Notion service added

--- src/main/java/ru/apexproject/service/NotionService.java
+++ src/main/java/ru/apexproject/service/NotionService.java
@@ -1,36 +1,37 @@
 package ru.apexproject.service;
 
-import com.basmingo.tasktonotion.config.NotionConfig;
-import com.basmingo.tasktonotion.dto.notion.Task;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import ru.apexproject.config.ApplicationConfig;
+import ru.apexproject.dto.notion.Task;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-@Service
 public class NotionService {
-    private final RestTemplate restTemplate;
-    NotionConfig notionConfig;
+    ApplicationConfig applicationConfig;
+    URL url;
+    HttpURLConnection httpURLConnection;
+    ObjectOutputStream out;
 
-    @Autowired
-    public NotionService(NotionConfig notionConfig) {
-        this.notionConfig = notionConfig;
-        this.restTemplate = new RestTemplate();
+    public NotionService(ApplicationConfig applicationConfig) throws IOException {
+        this.applicationConfig = applicationConfig;
+        this.url = new URL(applicationConfig.getNotionApiURI());
+        this.httpURLConnection = (HttpURLConnection) url.openConnection();
     }
 
-    public void send(Task task) {
-        HttpEntity<Task> entity = new HttpEntity<>(task, getHttpHeaders());
-        restTemplate.exchange("https://api.notion.com/v1/pages", HttpMethod.POST, entity, String.class);
-    }
+    public void doPost(Task task)  {
+        try {
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("Authorization", "Bearer " + applicationConfig.getNotionToken());
+            httpURLConnection.setRequestProperty("Notion-Version", "2022-06-28");
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
 
-    private HttpHeaders getHttpHeaders() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setBearerAuth(notionConfig.getNotionToken());
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.set("Notion-Version", "2022-06-28");
-        return httpHeaders;
+            httpURLConnection.setDoOutput(true);
+            out = new ObjectOutputStream(httpURLConnection.getOutputStream());
+            out.writeObject(task);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
