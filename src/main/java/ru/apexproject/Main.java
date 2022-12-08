16e24394cc2969960214a06a3008061dfdc4f1d3
Pavel Basmanov <pavel.basmanov@apex-project.ru>
2022-12-08 22:36:32 +0300

    Entry point added

--- src/main/java/ru/apexproject/Main.java
+++ src/main/java/ru/apexproject/Main.java
@@ -1,7 +1,17 @@
 package ru.apexproject;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.apexproject.service.TelegramBot;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
