8e2e4e9255174605bb357bfc75a21a85510d027f
basmingo <pabasmanov@gmail.com>
2022-12-11 22:07:16 +0300

    working wersion

--- src/main/java/ru/apexproject/service/NotionService.java
+++ src/main/java/ru/apexproject/service/NotionService.java
@@ -1,40 +1,61 @@
 package ru.apexproject.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import ru.apexproject.config.ApplicationConfig;
 import ru.apexproject.dto.notion.Task;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 /**
  *Sends a post request with Task object to Notion server
  */
+@Slf4j
 public class NotionService {
-    ApplicationConfig applicationConfig;
-    HttpURLConnection httpURLConnection;
-    ObjectOutputStream out;
-    URL url;
+    private final ApplicationConfig applicationConfig;
+    private URL url;
+    private HttpURLConnection con;
 
-    public NotionService(ApplicationConfig applicationConfig) throws IOException {
+    public NotionService(ApplicationConfig applicationConfig) {
         this.applicationConfig = applicationConfig;
-        this.url = new URL(applicationConfig.getNotionApiURI());
-        this.httpURLConnection = (HttpURLConnection) url.openConnection();
     }
 
-    public void doPost(Task task)  {
+    public void sendPost(Task task)  {
         try {
-            httpURLConnection.setRequestMethod("POST");
-            httpURLConnection.setRequestProperty("Authorization", "Bearer " + applicationConfig.getNotionToken());
-            httpURLConnection.setRequestProperty("Notion-Version", "2022-06-28");
-            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            url = new URL(applicationConfig.getNotionApiURI());
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Authorization", "Bearer " + applicationConfig.getNotionToken());
+            con.setRequestProperty("Notion-Version", "2022-06-28");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("Content-Type", "application/json");
 
-            httpURLConnection.setDoOutput(true);
-            out = new ObjectOutputStream(httpURLConnection.getOutputStream());
-            out.writeObject(task);
-            out.close();
+            con.setDoInput(true);
+            con.setDoOutput(true);
+
+            OutputStream output = con.getOutputStream();
+
+            ObjectMapper mapper = new ObjectMapper();
+            output.write(mapper
+                    .writeValueAsString(task)
+                    .getBytes(StandardCharsets.UTF_8));
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            output.close();
+
         } catch (IOException e) {
-            e.printStackTrace();
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()))){
+                String line;
+                while ((line = in.readLine()) != null) {
+                    log.error(line);
+                }
+            } catch (IOException p) {
+                log.error(e.getMessage());
+            }
+            log.error(e.getMessage());
         }
     }
 }
