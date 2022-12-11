c37d29b1de440fa82ff58d7e2b567ef0fe96ac27
basmingo <pabasmanov@gmail.com>
2022-12-11 22:06:16 +0300

    logging

--- src/main/java/ru/apexproject/Main.java
+++ src/main/java/ru/apexproject/Main.java
@@ -1,17 +1,19 @@
 package ru.apexproject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.apexproject.service.TelegramBot;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new TelegramBot());
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
         }
     }
 }
