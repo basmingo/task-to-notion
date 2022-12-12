a1a5fbc9f3bd8dbee2c17c295a7a921041b025ef
Pavel Basmanov <pavel.basmanov@apex-project.ru>
2022-12-12 21:49:49 +0300

    recover loading of properties added

--- src/main/java/ru/apexproject/config/ApplicationConfig.java
+++ src/main/java/ru/apexproject/config/ApplicationConfig.java
@@ -9,6 +9,10 @@ import java.util.Properties;
 @Getter
 @Slf4j
 public class ApplicationConfig {
+    private static final String PROPERTIES_NAME = "/application.properties";
+    private static final String RECOVER_PROPERTIES_NAME = "application.properties";
+    private final File location;
+    private final String parentLocation;
     private final Properties properties;
     private final String botName;
     private final String botToken;
@@ -19,12 +23,9 @@ public class ApplicationConfig {
 
     public ApplicationConfig() {
         properties = new Properties();
-        try (InputStream input = new FileInputStream("application.properties")){
-            properties.load(input);
-
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+        location = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        parentLocation = location.getParentFile().getAbsolutePath();
+        loadProperties();
 
         this.chatsDb = properties.getProperty("database.location");
         this.botName = properties.getProperty("bot.name");
@@ -32,6 +33,25 @@ public class ApplicationConfig {
         this.notionToken = properties.getProperty("notion.token");
         this.notionApiURI = properties.getProperty("notion.pages_api_uri");
         this.notionVersion = properties.getProperty("notion.version");
+    }
 
+    private void loadProperties() {
+        try (InputStream input = new FileInputStream(parentLocation + PROPERTIES_NAME)) {
+            properties.load(input);
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            loadRecoverProperties();
+        }
+    }
+
+    private void loadRecoverProperties() {
+        try (InputStream input = this.getClass().getClassLoader().getResourceAsStream(RECOVER_PROPERTIES_NAME)) {
+            properties.load(input);
+            log.info("recover properties loaded");
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
 }
